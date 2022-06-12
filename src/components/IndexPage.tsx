9ca61ddac13bd589c459f6ed7c5d9d73d274c529
ShotaroKataoka <s183347@stn.nagaokaut.ac.jp>
2022-06-12 21:18:41 +0900

    👍 index.tsx

--- src/components/IndexPage.tsx
+++ src/components/IndexPage.tsx
@@ -1,10 +1,18 @@
-import { Typography } from "@mui/material"
+import { Box, Typography } from "@mui/material"
 import { ConfigJson } from "../lib/api/config"
+import Link from 'next/link'
 
 export const IndexPage = ({ config }: { config: ConfigJson }) => {
     return (
-        <Typography variant="h1" color="text.secondary" align="center">
-            {config.blog_title}
-        </Typography>
+        <div>
+            <Typography variant="h1" color="text.secondary" align="center">
+                {config.blog_title}
+            </Typography>
+            <Box sx={{m: '3rem'}}/>
+            <Typography variant="body1" align="center">
+                This is simplist template design of RibbonCMS Blog.<br />
+                articles page is <Link href={`/articles`} passHref>here</Link>.
+            </Typography>
+        </div>
     )
 }
