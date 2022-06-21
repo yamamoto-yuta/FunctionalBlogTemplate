6d85b523f223c7c204e865d75da372042b5899f9
ShotaroKataoka <s183347@stn.nagaokaut.ac.jp>
2022-06-21 11:27:21 +0900

    🐛 basePath

--- src/lib/consts.js
+++ src/lib/consts.js
@@ -1,6 +1,6 @@
-export const rootPath = 'https://ribboncms.github.io/SimpleBlogTemplate'
-export const basePath = '/SimpleBlogTemplate'
-export const domainPath = 'ribboncms.github.io'
+const rootPath = 'https://ribboncms.github.io/SimpleBlogTemplate'
+const basePath = '/SimpleBlogTemplate'
+const domainPath = 'ribboncms.github.io'
 exports.rootPath = rootPath
 exports.basePath = basePath
 exports.domainPath = domainPath
