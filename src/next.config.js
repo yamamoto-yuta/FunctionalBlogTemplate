5a37682927614d3fbf060c28e8fcbb9c6ea091ed
ShotaroKataoka <s183347@stn.nagaokaut.ac.jp>
2022-06-21 10:59:42 +0900

    🐛 basePath

--- src/next.config.js
+++ src/next.config.js
@@ -1,9 +1,10 @@
+import { basePath } from './lib/consts'
+
 /** @type {import('next').NextConfig} */
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
-const { basePath } = require('./lib/consts')
 let config = {
   reactStrictMode: true,
   basePath: basePath
