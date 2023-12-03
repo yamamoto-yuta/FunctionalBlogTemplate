e52c493bace49b05127d09dd98a20954e746fc04
yamamoto-yuta <55144709+yamamoto-yuta@users.noreply.github.com>
2023-12-03 17:17:01 +0900

    Update datetime.ts

--- src/lib/datetime.ts
+++ src/lib/datetime.ts
@@ -14,23 +14,13 @@ export const formatDatetime = (dt: Date) => {
   const timezone = 9
 
   try {
-    // UTC -> JST
-    const datetime = new Date(
-      dt.getFullYear(),
-      dt.getMonth() + 1,
-      dt.getDate(),
-      dt.getHours() + timezone,
-      dt.getMinutes(),
-      dt.getSeconds(),
-    )
-
     // Format datetime string
-    const year = ('    ' + datetime.getFullYear()).slice(-4)
-    const month = ('00' + datetime.getMonth()).slice(-2)
-    const date = ('00' + datetime.getDate()).slice(-2)
-    const hours = ('00' + datetime.getHours()).slice(-2)
-    const minutes = ('00' + datetime.getMinutes()).slice(-2)
-    const seconds = ('00' + datetime.getSeconds()).slice(-2)
+    const year = ('    ' + dt.getFullYear()).slice(-4)
+    const month = ('00' + (dt.getMonth() + 1)).slice(-2) // 0~11 -> 1~12
+    const date = ('00' + dt.getDate()).slice(-2)
+    const hours = ('00' + (dt.getHours() + timezone)).slice(-2) // UTC 0> JST
+    const minutes = ('00' + dt.getMinutes()).slice(-2)
+    const seconds = ('00' + dt.getSeconds()).slice(-2)
     return `${year}年${month}月${date}日 ${hours}時${minutes}分`
   } catch {
     return ''
